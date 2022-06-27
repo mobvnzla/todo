9a7a17c21c698b8d9df3c381bc3bdfe3efa97dfb
gilberto <gilbertogalindez96@gmail.com>
2022-06-26 20:23:36 -0400

    agregando manejo de estados al proyecto

--- src/App.js
+++ src/App.js
@@ -20,13 +20,7 @@ function App() {
       <TodoSearch />
 
       <TodoList>
-        {todos.map(todo => (
-          <TodoItem
-            key={todo.text}
-            text={todo.text}
-            completed={todo.completed}
-          />
-        ))}
+        {todos.map(todo => ( <TodoItem key={todo.text} text={todo.text} completed={todo.completed} />))}
       </TodoList>
 
       <CreateTodoButton />
--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -2,18 +2,22 @@ import React from 'react';
 import './TodoSearch.css';
 
 function TodoSearch() {
+  const [searchValue, setSearchValue] = React.useState('');
   
   const onSearchValueChange = (event) => {
-    console.log(event.target.value)
-  }
+    
+    setSearchValue(event.target.value);
+  };
 
-  return (
-    <input 
-    className="TodoSearch"
-    placeholder="Cebolla" 
-    onChange={onSearchValueChange}
-    />
-  );
+  return [
+    <input
+      className="TodoSearch"
+      placeholder="Cebolla"
+      value={searchValue}
+      onChange={onSearchValueChange}
+    />,
+    <p>{searchValue}</p>
+  ];
 }
 
 export { TodoSearch };
