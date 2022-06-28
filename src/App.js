324d50a23cea4a6fdaf780e41f2e4e6a173f7fdd
gilberto <gilbertogalindez96@gmail.com>
2022-06-27 21:28:39 -0400

    haciendo funcional el componente todoSearch

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { TodoCounter } from './TodoCounter';
 import { TodoSearch } from './TodoSearch';
 import { TodoList } from './TodoList';
@@ -6,21 +6,46 @@ import { TodoItem } from './TodoItem';
 import { CreateTodoButton } from './CreateTodoButton';
 import './App.css';
 
-const todos = [
+const defaultTodos = [
   { text: 'Cortar cebolla', completed: true },
   { text: 'Tomar el cursso de intro a React', completed: false },
-  { text: 'Llorar con la llorona', completed: false },
+  { text: 'Llorar con la llorona', completed: true },
   { text: 'LALALALAA', completed: false },
 ];
 
 function App() {
+  const [todos, setTodos] = React.useState(defaultTodos);
+  const [searchValue, setSearchValue] = React.useState('');
+
+  const completedTodos = todos.filter(todo => !!todo.completed).length;
+  const totalTodos = todos.length;
+
+  let searchedTodos = [];
+
+  // Lógica para filtrar
+  if (!searchValue.length >= 1) {
+    searchedTodos = todos;
+  } else {
+    searchedTodos = todos.filter(todo => {
+      const todoText = todo.text.toLowerCase();
+      const searchText = searchValue.toLowerCase();
+      return todoText.includes(searchText);
+    });
+  }
+
   return (
     <React.Fragment>
-      <TodoCounter />
-      <TodoSearch />
+      <TodoCounter 
+        total={totalTodos}
+        completed={completedTodos}
+      />
+      <TodoSearch 
+      searchValue={searchValue}
+      setSearchValue={setSearchValue}
+      />
 
       <TodoList>
-        {todos.map(todo => ( <TodoItem key={todo.text} text={todo.text} completed={todo.completed} />))}
+        {searchedTodos.map(todo => ( <TodoItem key={todo.text} text={todo.text} completed={todo.completed} />))}
       </TodoList>
 
       <CreateTodoButton />
